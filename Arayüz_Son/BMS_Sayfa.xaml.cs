ae5350c4625a7ad4ff7a60d2bb47aca885de660c
NafIzEren <153806211+NafIzEren@users.noreply.github.com>
2023-12-14 10:55:04 +0300

    Duscart

---
+++ Arayüz_Son/BMS_Sayfa.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Arayüz_Son
+{
+    /// <summary>
+    /// BMS_Sayfa.xaml etkileşim mantığı
+    /// </summary>
+    public partial class BMS_Sayfa : Page
+    {
+        public BMS_Sayfa()
+        {
+            InitializeComponent();
+        }
+    }
+}
